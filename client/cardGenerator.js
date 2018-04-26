531d7a4a0f2ab66f42d24b9a55abbe81e6423a13
benji1703 <benjamin.arbibe@post.idc.ac.il>
2018-04-26 21:52:54 +0300

    working cards

--- client/cardGenerator.js
+++ client/cardGenerator.js
@@ -1,32 +1,53 @@
-AFRAME.registerComponent('playcard', {
-    schema: {
-    name: {type: 'string', default: '3C'}
-    },
+AFRAME.registerComponent("playcard", {
+  schema: {
+    name: { type: "string", default: "3C" },
+  },
 
-    getFront: function (name) {
-        let prefix = './Assets/cards-svg/';
-        let result = prefix.concat(name)
-        let sufix = '.svg'
-        return result.concat(sufix);
-    },
-  
-    init: function () {
-      
-        let data = this.data;
-        let el = this.el;
-    
-        this.geometryFront = new THREE.PlaneGeometry( 90, 110, 1, 1 );            
-        this.geometryBack = new THREE.PlaneGeometry( 90, 110, 1, 1 );            
-        this.geometryBack.applyMatrix( new THREE.Matrix4().makeRotationY( Math.PI ) );
-        this.textureFront = new THREE.ImageUtils.loadTexture( getFront(this.data.name) );    
-        this.textureBack = new THREE.ImageUtils.loadTexture( './Assets/cards-svg/Back.svg' );      
-  
-        let playcard = new.THREE.Object3D();
-        var meshFront = new THREE.Mesh( this.geometryFront, this.materialFront );
-        playcard.add(meshFront);
-        var meshBack = new THREE.Mesh( geometry2, material2 );
-        playcard.add(meshBack);
+  getFront: function(name) {
+    let prefix = "./Assets/cards-svg/";
+    let result = prefix.concat(name);
+    let sufix = ".svg";
+    return result.concat(sufix);
+  },
 
-        el.setObject3D('mesh', this.mesh);
+  init: function() {
+    let data = this.data;
+    let el = this.el;
+
+    this.geometry = new THREE.PlaneGeometry(6.35, 8.89, 1, 1);
+
+    this.textureFront = new THREE.TextureLoader().load(
+      this.getFront(this.data.name).toString(),
+    );
+    this.textureBack = new THREE.TextureLoader().load(
+      "./Assets/cards-svg/Back.svg",
+    );
+
+    this.materials = [
+      new THREE.MeshBasicMaterial({
+        map: this.textureFront,
+        side: THREE.FrontSide,
+      }),
+      new THREE.MeshBasicMaterial({
+        map: this.textureBack,
+        side: THREE.BackSide,
+      }),
+    ];
+
+    for (var i = 0, len = this.geometry.faces.length; i < len; i++) {
+      var face = this.geometry.faces[i].clone();
+      face.materialIndex = 1;
+      this.geometry.faces.push(face);
+      this.geometry.faceVertexUvs[0].push(this.geometry.faceVertexUvs[0][i].slice(0));
     }
-  });
+
+    console.log(this);
+
+    var playcard = new THREE.Object3D();
+    var mesh = new THREE.Mesh(this.geometry, new THREE.MeshFaceMaterial(this.materials));
+    playcard.add(mesh);
+
+    console.log(this.el.object3D);
+    el.setObject3D("mesh", playcard);
+  },
+});

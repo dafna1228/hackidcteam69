83457c13ef77a3d82f2a10e4063bb2b0097d9730
danpollak <dandan888@gmail.com>
2018-04-26 20:46:29 +0300

    basic graphic client

--- client/clientSocket.js
+++ client/clientSocket.js
@@ -1,20 +1,16 @@
 let socket = io('http://localhost');
 socket.on("playerLogin", (data) =>{
-    const playerManager = AFRAME.scenes[0].component.playerManager;
+    const playerManager = AFRAME.scenes[0].components['player-manager'];
+    playerManager.addPlayer(data);
     playerManager.setPlayerName(data.playerName, data.playerId);
 })
 socket.on("removePlayer", (data) =>{
-    const playerManager = AFRAME.scenes[0].component.playerManager;
+    const playerManager = AFRAME.scenes[0].components['player-manager'];
 
     playerManager.removePlayer(data);
 })
-socket.on("addPlayer", (data) =>{
-    const playerManager = AFRAME.scenes[0].component.playerManager;
-
-    playerManager.addPlayer(data);
-})
 socket.on("updateGameData", (data) => {
-    const playerManager = AFRAME.scenes[0].component.playerManager;
+    const playerManager = AFRAME.scenes[0].components['player-manager'];
 
     playerManager.updateGameData(data.gameData);  
 })

f0784719167d4cca9af72446868639a81de2cbd8
danpollak <dandan888@gmail.com>
2018-04-27 05:36:09 +0300

    player cards available

--- client/tableCenter.js
+++ client/tableCenter.js
@@ -9,34 +9,28 @@ AFRAME.registerComponent("table-center", {
         turn: {x: 0, card: "5S", turned: false },
         river: {x: 0, card: "6S", turned: false },
       }
-    }
+    },
+    visible:{default: true}
   },
 
   init: function() {
-    // this.putCards = AFRAME.utils.bind(this.putCards, this);
-    const data = this.data;
+  },
+
+  update: function() {
     const el = this.el;
-    // create card row
+    const {cards, visible} = this.data;
     CARD_ORDER.forEach((card, index)=>{
       let cardEntity = document.createElement('a-entity');
       cardEntity.setAttribute('id', card);
       cardEntity.setAttribute('name', card);
       cardEntity.setAttribute('position', {x: index -3 + (index*0.5), y: 0, z: 0,});
-      // console.log('turned card', this.data.cards[card].turned ? { x: 90, y: 0, z: 0 } : { x: -90, y: 0, z: 0 });
-      cardEntity.setAttribute("rotation", this.data.cards[card].turned ? { x: 90, y: 0, z: 0 } : { x: -90, y: 0, z: 0 });
-      cardEntity.setAttribute("playcard", {name: this.data.cards[card].card});
+      cardEntity.setAttribute("rotation", cards[card].turned ? { x: 90, y: 0, z: 0 } : { x: -90, y: 0, z: 0 });
+      cardEntity.setAttribute("playcard", {name: cards[card].card});
       this.el.appendChild(cardEntity);
       const playerPosition = document.getElementById('player').getAttribute('position');
-      this.el.setAttribute("look-at",{x: playerPosition.x, y: playerPosition.y + 0.5, z: playerPosition.z});
     });
-  },
-
-  update: function() {
-    // CARD_ORDER.forEach((card, index)=>{
-    //   let cardEntity = document.getElementById(card);
-    //   if(card){
-    //     cardEntity.setAttribute("rotation", card.turned ? { x: 90, y: 0, z: 0 } : { x: -90, y: 0, z: 0 });
-    //   }
-    // });;
+    const playerPosition = document.getElementById('player').getAttribute('position');    
+    this.el.setAttribute("look-at",{x: playerPosition.x, y: playerPosition.y + 0.5, z: playerPosition.z});
+    this.el.setAttribute('visible', visible);
   }
 });

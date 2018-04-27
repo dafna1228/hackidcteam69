4aa40154abba2d478d24e8b274bb239cd66ee3b3
danpollak <dandan888@gmail.com>
2018-04-27 05:08:59 +0300

    table cards appear nicely for each player

--- client/tableCenter.js
+++ client/tableCenter.js
@@ -1,11 +1,15 @@
 const CARD_ORDER = ['flopA','flopB','flopC','turn','river'];
 AFRAME.registerComponent("table-center", {
   schema: {
-    flopA: {x: 0, card: "2S", turned: true },
-    flopB: {x: 0, card: "3S", turned: true },
-    flopC: {x: 0, card: "4S", turned: false },
-    turn: {x: 0, card: "5S", turned: false },
-    river: {x: 0, card: "6S", turned: false },
+    cards:{
+      default:{
+        flopA: {x: 0, card: "2S", turned: true },
+        flopB: {x: 0, card: "3S", turned: true },
+        flopC: {x: 0, card: "4S", turned: false },
+        turn: {x: 0, card: "5S", turned: false },
+        river: {x: 0, card: "6S", turned: false },
+      }
+    }
   },
 
   init: function() {
@@ -18,18 +22,21 @@ AFRAME.registerComponent("table-center", {
       cardEntity.setAttribute('id', card);
       cardEntity.setAttribute('name', card);
       cardEntity.setAttribute('position', {x: index -3 + (index*0.5), y: 0, z: 0,});
-      cardEntity.setAttribute("rotation", card.turned ? { x: 90, y: 0, z: 0 } : { x: -90, y: 0, z: 0 });
-      cardEntity.setAttribute("playcard", {name: card.card});
+      // console.log('turned card', this.data.cards[card].turned ? { x: 90, y: 0, z: 0 } : { x: -90, y: 0, z: 0 });
+      cardEntity.setAttribute("rotation", this.data.cards[card].turned ? { x: 90, y: 0, z: 0 } : { x: -90, y: 0, z: 0 });
+      cardEntity.setAttribute("playcard", {name: this.data.cards[card].card});
       this.el.appendChild(cardEntity);
+      const playerPosition = document.getElementById('player').getAttribute('position');
+      this.el.setAttribute("look-at",{x: playerPosition.x, y: playerPosition.y + 0.5, z: playerPosition.z});
     });
   },
 
   update: function() {
-    CARD_ORDER.forEach((card, index)=>{
-      let cardEntity = document.getElementById(card);
-      if(card){
-        cardEntity.setAttribute("rotation", card.turned ? { x: 90, y: 0, z: 0 } : { x: -90, y: 0, z: 0 });
-      }
-    });;
+    // CARD_ORDER.forEach((card, index)=>{
+    //   let cardEntity = document.getElementById(card);
+    //   if(card){
+    //     cardEntity.setAttribute("rotation", card.turned ? { x: 90, y: 0, z: 0 } : { x: -90, y: 0, z: 0 });
+    //   }
+    // });;
   }
 });

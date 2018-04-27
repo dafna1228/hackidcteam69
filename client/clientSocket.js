f5c90a9b3155fd36d0c71e0d5ebfea8c98f342c5
danpollak <dandan888@gmail.com>
2018-04-27 04:10:34 +0300

    WIP cardGenerator rewrite

--- client/clientSocket.js
+++ client/clientSocket.js
@@ -7,13 +7,34 @@ socket.on("playerLogin", (data) =>{
     playerManager.addPlayer(data);
     playerManager.setPlayerName(data);
 })
+socket.on("startGame", (data) => {
+    // Lock Add Players
+    // Add Spectators?
+});
+socket.on("startRound", (data) => {
+    playerManager.dealCards(data);
+    playerManager.revealTableCenter('flop');    
+})
+socket.on("playerTurn", (data)=> {
+    player.updateChips(data);
+    playerManager.runTurn(data);
+})
+socket.on("revealTurn", (data) => {
+    playerManager.revealTableCenter('turn');
+});
+socket.on("revealRiver", (data) => {
+    playerManager.revealTableCenter('river');
+});
+socket.on("endRound", (data) => {
+    playerManager.showWinner(data);
+    playerManager.discardCards(data);
+    playerManager.updateChips(data);
+})
 socket.on("removePlayer", (data) =>{
     const playerManager = AFRAME.scenes[0].components['player-manager'];
-
     playerManager.removePlayer(data);
 })
 socket.on("updatePlayerRotation", (data) => {
     const playerManager = AFRAME.scenes[0].components['player-manager'];
     playerManager.updatePlayerRotation(data);  
 })
-//TODO: Add a listener for player leaving
--- client/tableCenter.js
+++ client/tableCenter.js
@@ -1,76 +1,35 @@
+const CARD_ORDER = ['flopA','flopB','flopC','turn','river'];
 AFRAME.registerComponent("table-center", {
   schema: {
-    layout: {
-      type: "array",
-      default: [
-        { i: "a", x: 0, y: 0, w: 1, card: "2S", turned: false },
-        { i: "b", x: 1, y: 0, w: 1, card: "3S", turned: false },
-        { i: "c", x: 2, y: 0, w: 1, card: "4S", turned: true },
-        { i: "d", x: 3, y: 0, w: 1, card: "5S", turned: false },
-        { i: "e", x: 4, y: 0, w: 1, card: "6S", turned: false },
-      ],
-    },
-    marginColumn: { default: 0.3, min: 0, max: 1 },
+    flopA: {x: 0, card: "2S", turned: true },
+    flopB: {x: 0, card: "3S", turned: true },
+    flopC: {x: 0, card: "4S", turned: false },
+    turn: {x: 0, card: "5S", turned: false },
+    river: {x: 0, card: "6S", turned: false },
   },
 
   init: function() {
-    this.convertLayout = AFRAME.utils.bind(this.convertLayout, this);
-    this.putCards = AFRAME.utils.bind(this.putCards, this);
+    // this.putCards = AFRAME.utils.bind(this.putCards, this);
+    const data = this.data;
+    const el = this.el;
+    // create card row
+    CARD_ORDER.forEach((card, index)=>{
+      let cardEntity = document.createElement('a-entity');
+      cardEntity.setAttribute('id', card);
+      cardEntity.setAttribute('name', card);
+      cardEntity.setAttribute('position', {x: index -3 + (index*0.5), y: 0, z: 0,});
+      cardEntity.setAttribute("rotation", card.turned ? { x: 90, y: 0, z: 0 } : { x: -90, y: 0, z: 0 });
+      cardEntity.setAttribute("playcard", {name: card.card});
+      this.el.appendChild(cardEntity);
+    });
   },
 
   update: function() {
-    this.removeTiles();
-    this.convertLayout();
-    this.putCards();
-  },
-
-  // Creating the layout for every child
-  convertLayout: function() {
-    let margC = this.data.marginColumn;
-    this.aframeLayout = this.data.layout.map(function(child) {
-      return {
-        i: child.i,
-        x: child.x + 0.5 * child.w + margC * child.x,
-        y: child.y,
-        card: child.card,
-        turned: child.turned,
-      };
-    });
-  },
-
-  putCards: function() {
-    const self = this;
-    let margC = this.data.marginColumn;
-    this.aframeLayout.map(function(child) {
-      let childEntity = document.createElement("a-entity");
-      childEntity.setAttribute("id", child.i);
-
-      AFRAME.utils.entity.setComponentProperty(childEntity, "position", {
-        x: child.x - (2.5 + 2 * margC), // Adding the math to center it (with margin)
-        y: child.y, // There is 12/2 column and 11/2 margin needed to shift
-        z: 1, // Fixing the curve radius
-      });
-
-      AFRAME.utils.entity.setComponentProperty(childEntity, "playcard", {
-        name: child.card,
-      });
-
-      childEntity.setAttribute("rotation", child.turned ? { x: 90, y: 0, z: 0 } : { x: -90, y: 0, z: 0 });
-
-      self.el.appendChild(childEntity);
-    });
-  },
-
-  removeTiles: function() {
-    while (this.el.firstChild) {
-      this.el.removeChild(this.el.firstChild);
-    }
-  },
-
-  remove: function() {
-    this.curve = null;
-    if (this.el.getObject3D("clones")) {
-      this.el.removeObject3D("clones");
-    }
-  },
+    CARD_ORDER.forEach((card, index)=>{
+      let cardEntity = document.getElementById(card);
+      if(card){
+        cardEntity.setAttribute("rotation", card.turned ? { x: 90, y: 0, z: 0 } : { x: -90, y: 0, z: 0 });
+      }
+    });;
+  }
 });
